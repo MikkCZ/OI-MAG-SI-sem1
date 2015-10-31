55305048cd9f41a1bc750af7e3ec4c6f3521c1da
MikkCZ <michal.stanke@mikk.cz>
2015-10-31 11:37:28 +0100

    Add PAL_02 - Reverse an Edge project

---
+++ PAL/PAL_02/src/main/java/pal/ReverseEdge.java
@@ -0,0 +1,14 @@
+package pal;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class ReverseEdge implements Task {
+
+    @Override
+    public String eval(InputStream stdin) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
